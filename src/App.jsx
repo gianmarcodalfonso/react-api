680a3329a5714c120daa851f7d0199e673092146
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-02 16:12:15 +0200

    feat: milestone-1(actressesLink fetch)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const actressesLink = `https://lanciweb.github.io/demo/api/actresses/`;
+
 function App() {
-	return <></>;
+	const [actresses, setActresses] = useState([]);
+	const fetchActressesList = () => {
+		axios.get(actressesLink).then((res) => {
+			console.log(res.data);
+			setActresses(res.data);
+		});
+	};
+
+	useEffect(fetchActressesList, []);
+
+	return (
+		<>
+			<header></header>
+			<main></main>
+		</>
+	);
 }
 
 export default App;
